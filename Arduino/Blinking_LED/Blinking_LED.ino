16bed6818d5e673c1ebdba5930e15e5933c144ae
Muhirwa Thierry <mithierry1010@gmail.com>
2024-04-12 15:53:21 +0200

    Blinking LED project

---
+++ Arduino/Blinking_LED/Blinking_LED.ino
@@ -0,0 +1,14 @@
+int LEDPin = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LEDPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LEDPin, HIGH);
+  delay(500);
+  digitalWrite(LEDPin, LOW);
+  delay(500);
+}
