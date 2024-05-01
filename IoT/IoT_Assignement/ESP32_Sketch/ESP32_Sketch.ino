6035026a7cc82139454eac597e1962cae376ab1b
Muhirwa Ishimwe Thierry <mithierry1010@gmail.com>
2024-05-01 15:48:37 +0300

    Update ESP32_Sketch.ino
    Added explanatory comments

--- IoT/IoT_Assignement/ESP32_Sketch/ESP32_Sketch.ino
+++ IoT/IoT_Assignement/ESP32_Sketch/ESP32_Sketch.ino
@@ -6,20 +6,26 @@
 
 // Declaration part
 int DHTDataPin = 5;
+
+// TCP port and IP address server is running on.
 int port = 8080;
 const char* ipAddress = "192.168.198.35";
 
+// SSID and password of Wi-Fi network you're using.
 const char* ssid = "Wi-Fi";
 const char* password = "ThierryTeach";
 
+// Declaration of objects that are going to be used in this project
 WiFiClient server;
 DHT dht(DHTDataPin, DHT11);
 std::ostringstream jsonSerializer;
 
 void setup() {
-  // put your setup code here, to run once:
+  // Initialization and allocation of resource we need.
   Serial.begin(9600);
   WiFi.mode(WIFI_STA);
+
+  // Connecting to the Wi-Fi network.
   WiFi.begin(ssid, password);
   Serial.println("\nConnecting to the WiFi Network.");
   while (WiFi.status() != WL_CONNECTED) {
@@ -34,21 +40,25 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Reading temperature and humidity from DHT11 sensor we have
   float temp = dht.readTemperature();
   float hum = dht.readHumidity();
   const char* land_plot = "KN87 St7 Kigali";
 
+  // Serializing data into json format
   jsonSerializer << "{ \"land_plot\":\"" << land_plot
                  << "\", \"temperature\": " << temp
                  << ", \"humidity\": " << hum << "}\n";
 
+  // Sending serialized data to the server
   if (server.connect(ipAddress, port)) {
     if (server.connected()) {
       server.write(jsonSerializer.str().c_str());
       //Serial.println(jsonSerializer.str().c_str());
     }
   }
+
+  // Clearing the stringstream we're using in serialization.
   jsonSerializer.str("");
   jsonSerializer.clear();
   delay(5000);
